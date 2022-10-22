866333f75c7c848292370f7d5bf5a5ac87c789ad
“lzk” <657525050@qq.com>
2022-10-22 17:35:26 +0800

    commit

--- src/main/java/lzktest/commonlog/CommonsLogger.java
+++ src/main/java/lzktest/commonlog/CommonsLogger.java
@@ -10,7 +10,7 @@ public class CommonsLogger {
 	public static void main(String[] args) {
 		logger.debug("DEBUG ...");
 		logger.info("INFO ...");
-		logger.error("ERROR ...。。");
+		logger.error("ERROR ..");
 
 	}
 
